9903dc2d449eedb3a91b8ce647ceb261b189c59b
SushreeLitun <64742363+SushreeLitun@users.noreply.github.com>
2020-05-07 20:41:11 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,4 @@
-makeCacheMatrix <- function(x = matrix()) {
-  ##This function Function makeCacheMatrix gets a matrix as an input, set the value of the matrix,
+  ##The function makeCacheMatrix gets a matrix as an input, set the value of the matrix,
   #get the value of the matrix, set the inverse Matrix and get the inverse Matrix. The matrix object
   #can cache its own object. 
   
@@ -24,7 +23,13 @@ list(setMatrix = setMatrix, getMatrix = getMatrix,
 
 }
 
-## Write a short comment describing this function
+## The function cacheSolve takes the output of the previous matrix makeCacheMatrix(matrix) as an 
+# input and checks inverse matrix from makeCacheMatrix(matrix) has any value in it or not.
+# In case inverse matrix from makeCacheMatrix((matrix) is empty, it gets the original matrix data from 
+# and set the invertible  matrix by using the solve function.
+# In case inverse matrix from makeCacheMatrix((matrix) has some value in it (always works
+#after running the code 1st time), it returns a message  "Getting Cached Invertible Matrix" 
+#and the cached object
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
